dc796e3c68d6a2e1188ab0a119cbfab96cfbddfa
crimage <crimag3@gmail.com>
2017-04-28 12:17:50 +0200

    Fixing File Class

---
+++ CrimageGallery/Models/File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrimageGallery.Models
+{
+    public class File
+    {
+        public virtual int FileId { get; set; }
+
+        public virtual string FileName { get; set; }
+
+        public virtual string ContentType { get; set; }
+
+        public virtual byte[] Content { get; set; }
+    }
+}
