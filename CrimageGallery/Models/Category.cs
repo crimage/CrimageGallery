8a5039e21a742950a54384f28c97356802238a3a
crimage <crimag3@gmail.com>
2017-04-28 12:26:56 +0200

    Adding Image and Category Class

---
+++ CrimageGallery/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrimageGallery.Models
+{
+    public class Category
+    {
+        public virtual int CategoryId { get; set; }
+        
+        public virtual string Name { get; set; } 
+
+        public virtual CategoryImageFile Image { get; set; }
+    }
+}
---
+++ CrimageGallery/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrimageGallery.Models
+{
+    public class Image
+    {
+        public virtual int ImageId { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual UserImageFile UserImage { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
